734458ac3f156aa04ad7e7e0c1d7b1aae955687d
shagbag913 <sh4gbag913@gmail.com>
2019-10-02 19:33:09 -0400

    BoardConfigCommon: use debug defconfig on !user builds

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,7 +130,11 @@ endif
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+TARGET_KERNEL_CONFIG := marlin-debug_defconfig
+else
 TARGET_KERNEL_CONFIG := marlin_defconfig
+endif
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
