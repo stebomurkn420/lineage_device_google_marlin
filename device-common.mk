4f0d6f60b8f159cd1d224b1ebbc7fb4f012ef027
stebomurkn420 <stevn4127@gmail.com>
2020-04-02 15:02:55 -0400

    Build with gapps

--- device-common.mk
+++ device-common.mk
@@ -51,6 +51,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.service.debuggable=1 \
     persist.service.adb.enable=1
 
+# Include Google apps by default
+ifneq ($(TARGET_BUILD_GAPPS),false)
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+endif
+
 PRODUCT_SOONG_NAMESPACES += \
     device/google/marlin \
     vendor/google/camera \
