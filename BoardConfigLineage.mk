783ef73476542bfac45937f48e04bc9eafa4b72a
stebomurkn420 <stevn4127@gmail.com>
2020-04-02 15:05:32 -0400

    AOSP surfaceflinger

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -9,5 +9,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_USE_AOSP_SURFACEFLINGER := true
 
 BUILD_BROKEN_DUP_RULES := true
